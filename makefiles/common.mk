02fd1b00bf3a9eb1afa6ceb266581a9828e213c9
Tyler H <tyhoffman1@gmail.com>
2014-01-20 23:44:26 -0500

    added my code in top of common

--- makefiles/common.mk
+++ makefiles/common.mk
@@ -1,3 +1,21 @@
+# my code
+
+export THEOS_DEVICE_IP=192.168.1.130
+
+one : clean ddeb all package
+
+
+ddeb :
+	rm -f com.*
+
+upload :
+	ssh vps 'setopt NULL_GLOB; rm -f /var/www/cydia.tyhoffman.com/public_html/deb_files/*$(shell echo $(TWEAK_NAME) | tr A-Z a-z)*'
+	scp com.* vps:/var/www/cydia.tyhoffman.com/public_html/deb_files
+	ssh vps 'cd /var/www/cydia.tyhoffman.com; bash /var/www/cydia.tyhoffman.com/script.sh'
+
+
+# continue common.mk original code
+
 all::
 
 ifeq ($(notdir $(firstword $(SUDO_COMMAND))),make)
